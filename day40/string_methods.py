125c499f070af738cdb72fb28fe3904cbc32d5f5
Campbellquire23 <Campbell.quirebui@gmail.com>
2025-10-01 12:13:33 -0400

    Add files via upload
    Day 40 - String methods and slicing practice (Module 3)

---
+++ day40/string_methods.py
@@ -0,0 +1,11 @@
+text = "cybersecuritylearning"
+print("length:", len(text))
+print("lowercase:", text.lower())
+print("uppercase:", text.upper())
+slice_part = text[5:13]
+print("sliced part:", slice_part)
+index_of_learn = text.find("learning")
+print("index of 'learning:", index_of_learn)
+python3 string_methods.py
+export_file sting_methods.py
+
